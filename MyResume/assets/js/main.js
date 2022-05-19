e123242943c5282f116a71664d17cc179364cd8c
Erick Suzart Souza <ericksuzart@gmail.com>
2022-05-19 16:32:49 -0300

    Update

--- MyResume/assets/js/main.js
+++ MyResume/assets/js/main.js
@@ -219,10 +219,10 @@
    */
   new Swiper('.portfolio-details-slider', {
     speed: 400,
-    loop: true,
+    loop: false,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: true
     },
     pagination: {
       el: '.swiper-pagination',
